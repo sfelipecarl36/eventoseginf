77db88b35ab95aa54005f20d9d356fa652dedecf
Felipe <exper15gamer@gmail.com>
2023-05-17 14:57:31 -0300

    function navegar finish

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function navegar(url) {
+	console.log(url)
+	const target = document.getElementById(String(url))
+	console.log(target);
+	yTarget = target.offsetTop - document.body.scrollTop - 120;
+
+	console.log(yTarget);
 	
+	window.scrollTo({
+  		top: yTarget,
+  		behavior: 'smooth' 
+	});
 }
