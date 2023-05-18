df8287257c19939372fa004d26c1d825f552c8a8
FELIPE CARLOS RABELO DA SILVA <05274906281@trt8.jus.br>
2023-05-18 10:54:59 -0300

    mobile responsive

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var sidebarBol = false;
+var slideNum = 2;
+var slideIndex = 1;
+
 function navegar(url) {
 	console.log(url)
 	const target = document.getElementById(String(url))
@@ -8,6 +12,50 @@ function navegar(url) {
 	
 	window.scrollTo({
   		top: yTarget,
-  		behavior: 'smooth' 
+  		behavior: 'smooth'
 	});
 }
+
+function sidebarCheck() {
+
+	var sidebar = document.getElementById('sidebar');
+	var blackdrop = document.getElementById('blackdrop');
+	var menusidebar = document.getElementById('menu-sidebar');
+
+	if(sidebarBol) {
+		sidebarBol = false;
+		sidebar.style.width = '0px';
+		menusidebar.style.display = 'none'
+		menusidebar.style.opacity = '0'
+		blackdrop.style.display = 'none';
+		document.body.style.overflowY = 'visible';
+	}
+
+	else{
+		sidebarBol = true
+		sidebar.style.width = '260px';
+		menusidebar.style.display = 'block'
+		menusidebar.style.opacity = '1'
+		blackdrop.style.display = 'block';
+		document.body.style.overflowY = 'hidden';
+	}
+
+}
+
+function nextSlide() {
+
+	var imgbanner = document.getElementById('imgbanner');
+
+	if (slideIndex<slideNum) {
+		slideIndex+=1;
+		imgbanner.style.backgroundImage = 'url(img/banner-principal-'+slideIndex+'.jpg)';
+		setTimeout(nextSlide, 5000);
+	}
+	else {
+		slideIndex=1;
+		imgbanner.style.backgroundImage = 'url(img/banner-principal-'+slideIndex+'.jpg)';
+		setTimeout(nextSlide, 5000);
+	}
+}
+
+setTimeout(nextSlide, 5000);
